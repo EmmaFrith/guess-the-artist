1ba09772a1968deeedc561be6e7313256c368f5d
Emma Frith <emma.frith@education.gov.uk>
2024-05-09 12:14:40 +0100

    add alt text

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 /*-------------- Constants -------------*/
 
 const artists = [
-    { artist1: 'Van Gogh', artist2: 'Gauguin', artist3: 'Cézanne', artist4: 'Renoir', image: 'https://uploads7.wikiart.org/images/paul-gauguin/tahitian-mountains-1893.jpg!Large.jpg', correctArtist: 'Gauguin' },
-    { artist1: 'Klimt', artist2: 'Klee', artist3: 'Chagall', artist4: 'Miro', image: 'https://d3d00swyhr67nd.cloudfront.net/_source/artuk_stories/paulklee-725px-1.jpg', correctArtist: 'Klee' },
-    { artist1: 'Sisley', artist2: 'Manet', artist3: 'Monet', artist4: 'Seurat', image: 'https://d3d00swyhr67nd.cloudfront.net/w800h800/collection/NGS/NGS/NGS_NGS_NG_2235-001.jpg', correctArtist: 'Sisley' },
-    { artist1: 'Whistler', artist2: 'Monet', artist3: 'Pisarro', artist4: 'Turner', image: 'https://www.theartstory.org/images20/works/whistler_james_abbott_mcneill_2.jpg?1', correctArtist: 'Whistler' },
-    { artist1: 'Matisse', artist2: 'Derain', artist3: 'Delauney', artist4: 'Braque', image: 'https://d26jxt5097u8sr.cloudfront.net/s3fs-public/Full_matisse2.jpg', correctArtist: 'Derain' },
-    { artist1: 'Gore', artist2: 'Cezanne', artist3: 'Derain', artist4: 'Hockney', image: 'https://www.bookroomartpress.co.uk/wp-content/uploads/2022/09/The-Icknield-Way-Spencer-Frederick-Gore.jpg', correctArtist: 'Gore' },
-    { artist1: 'Matisse', artist2: 'Cezanne', artist3: 'Picasso', artist4: 'Braque', image: 'https://www.christies.com/-/jssmedia/images/features/articles/2021/11/the-cox-collection/paul-cezanne-cox-christies-main.jpg?h=953&iar=0&w=1200&rev=bbadaa3ed4a240babee9b8a4b046190b&hash=36c714b5e6e237a12bfe9dc99b0f2c2c6062bf0f', correctArtist: 'Cezanne' },
-    { artist1: 'Picasso', artist2: 'Dali', artist3: 'Kandinsky', artist4: 'Chagall', image: 'https://www.parkwestgallery.com/wp-content/uploads/2015/07/903005_laccordeoniste-m.-204-1957_chagall-jpg.jpg', correctArtist: 'Chagall' },
-    { artist1: 'Munch', artist2: 'Van Gogh', artist3: 'Pisarro', artist4: 'Seurat', image: 'https://sal-bucket-1.s3.amazonaws.com/wp-content/uploads/2020/06/30144802/Vincent_van_Gogh_-_The_Fields_1890.jpg', correctArtist: 'Van Gogh' },
-    { artist1: 'Matisse', artist2: 'Signac', artist3: 'Seurat', artist4: 'Cross', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Paul_Signac_%281863-1935%29%2C_L%E2%80%99estuaire_%C3%A0_Trieux%2C_Bretagne._Watercolour%2C_pencil_and_ink%2C_11.2_x_20_cm.jpg', correctArtist: 'Signac' }
+    { artist1: 'Van Gogh', artist2: 'Gauguin', artist3: 'Cézanne', artist4: 'Renoir', image: 'https://uploads7.wikiart.org/images/paul-gauguin/tahitian-mountains-1893.jpg!Large.jpg', correctArtist: 'Gauguin', alt: 'Colourful painting of Tahitian mountains.' },
+    { artist1: 'Klimt', artist2: 'Klee', artist3: 'Chagall', artist4: 'Miro', image: 'https://d3d00swyhr67nd.cloudfront.net/_source/artuk_stories/paulklee-725px-1.jpg', correctArtist: 'Klee', alt: 'Surreal and celestial composition featuring a house, clocktower and mountain.' },
+    { artist1: 'Sisley', artist2: 'Manet', artist3: 'Monet', artist4: 'Seurat', image: 'https://d3d00swyhr67nd.cloudfront.net/w800h800/collection/NGS/NGS/NGS_NGS_NG_2235-001.jpg', correctArtist: 'Sisley', alt: 'Impressionist painting of water in blues and greys.' },
+    { artist1: 'Whistler', artist2: 'Monet', artist3: 'Pisarro', artist4: 'Turner', image: 'https://www.theartstory.org/images20/works/whistler_james_abbott_mcneill_2.jpg?1', correctArtist: 'Whistler', alt: 'Muted beach landscape in sandy colours with hints of blue and purple, and a sail boat in the distance.' },
+    { artist1: 'Matisse', artist2: 'Derain', artist3: 'Delauney', artist4: 'Braque', image: 'https://d26jxt5097u8sr.cloudfront.net/s3fs-public/Full_matisse2.jpg', correctArtist: 'Derain', alt: 'Fauvist landscape with vidid trees and hills, painted in distinct strokes.'},
+    { artist1: 'Gore', artist2: 'Cezanne', artist3: 'Derain', artist4: 'Hockney', image: 'https://www.bookroomartpress.co.uk/wp-content/uploads/2022/09/The-Icknield-Way-Spencer-Frederick-Gore.jpg', correctArtist: 'Gore', alt: 'Landscape of hills and trees with saturated colours such a purple, pink and yellow.' },
+    { artist1: 'Matisse', artist2: 'Picasso', artist3: 'Cezanne', artist4: 'Braque', image: 'https://www.christies.com/-/jssmedia/images/features/articles/2021/11/the-cox-collection/paul-cezanne-cox-christies-main.jpg?h=953&iar=0&w=1200&rev=bbadaa3ed4a240babee9b8a4b046190b&hash=36c714b5e6e237a12bfe9dc99b0f2c2c6062bf0f', correctArtist: 'Cezanne', alt: 'Abstracted view of the sea and rooftops framed by trees.' },
+    { artist1: 'Picasso', artist2: 'Dali', artist3: 'Kandinsky', artist4: 'Chagall', image: 'https://www.parkwestgallery.com/wp-content/uploads/2015/07/903005_laccordeoniste-m.-204-1957_chagall-jpg.jpg', correctArtist: 'Chagall', alt: 'Surreal and celestial composition with reclining woman in fluid ink and watercolour or gouache.' },
+    { artist1: 'Munch', artist2: 'Van Gogh', artist3: 'Pisarro', artist4: 'Seurat', image: 'https://sal-bucket-1.s3.amazonaws.com/wp-content/uploads/2020/06/30144802/Vincent_van_Gogh_-_The_Fields_1890.jpg', correctArtist: 'Van Gogh', alt: 'Landscape with fields in blue, green and yellow in swervy brushstrokes.' },
+    { artist1: 'Matisse', artist2: 'Signac', artist3: 'Seurat', artist4: 'Cross', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Paul_Signac_%281863-1935%29%2C_L%E2%80%99estuaire_%C3%A0_Trieux%2C_Bretagne._Watercolour%2C_pencil_and_ink%2C_11.2_x_20_cm.jpg', correctArtist: 'Signac', alt: 'Fluid pen and ink drawing depicting seafront, sailboats and cliffs.' }
 ]
 
 /*---------- Variables (state) ---------*/
 
-
 let currentQuestionIndex = 0;
-//let userChoice = null;
 let score = 0;
 
 /*----- DOM/event listeners  -----*/
@@ -62,6 +60,7 @@ function goToNextQuestion() {
         option3.innerText = artists[currentQuestionIndex].artist3;
         option4.innerText = artists[currentQuestionIndex].artist4;
         img.src = artists[currentQuestionIndex].image;
+        img.alt = artists[currentQuestionIndex].alt;
         document.getElementById("correct").innerHTML = ""
     }
     else {
@@ -81,7 +80,7 @@ function endGame() {
 }
 
 function playAgain() {
-    currentQuestionIndex = 0 - 1;
+    currentQuestionIndex = - 1;
     score = 0;
     nextQuestionButton.classList.remove("hidden");
     playAgainButton.classList.add("hidden");
@@ -97,7 +96,6 @@ function checkIfCorrect() {
     option4.disabled = true;
     if (this.innerText === artists[currentQuestionIndex].correctArtist) {
         score = score + 1
-        console.log(score)
         scoreDisplay.innerHTML = score
         document.getElementById("correct").innerHTML = "Correct 🎉"
         positiveAudioPlayer.play()
